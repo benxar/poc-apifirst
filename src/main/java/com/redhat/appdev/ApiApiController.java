0130b9ef3a16817cb8c783434b61aba59857a996
benxar <benjasostaric@gmail.com>
2020-04-08 13:44:54 -0300

    initial import

---
+++ src/main/java/com/redhat/appdev/ApiApiController.java
@@ -0,0 +1,25 @@
+package com.redhat.appdev;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-04-08T13:34:57.043-03:00[America/Argentina/Buenos_Aires]")
+
+@Controller
+@RequestMapping("${openapi.new.base-path:}")
+public class ApiApiController implements ApiApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public ApiApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
